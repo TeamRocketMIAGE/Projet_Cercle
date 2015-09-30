639a0ec328d9d9fc9df5dbbb0c816272e5e76d89
sundwn <line011011@gmail.com>
2015-09-30 10:18:04 +0200

    First Commit

---
+++ src/main/java/main/mainController.java
@@ -0,0 +1,18 @@
+package main;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class mainController {
+	
+	@RequestMapping("/")
+    public String requestCreatePage() {
+    	
+
+    	return "index";
+    	
+    }
+
+}
