9352f8ac2af891ec2e634360c519b8924e03012a
sundwn <line011011@gmail.com>
2015-10-06 17:07:47 +0200

    Login ok.

--- src/main/java/main/mainController.java
+++ src/main/java/main/mainController.java
@@ -1,62 +1,53 @@
 package main;
 
-
-
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class mainController {
-	
-	 @RequestMapping(value="/home", method=RequestMethod.GET)
-    public String requestCreatePageIndex1() {
-    	
-		 System.out.println("request home get");
-    	return "index";
-    	
-    }
-	
-	@RequestMapping("/")
-    public String requestCreatePageIndex2() {
-    	
-		return "redirect:/home";
-    	
-    }
-	
 
-	
+	@RequestMapping(value = "/home", method = RequestMethod.GET)
+	public String requestCreatePageIndex1() {
+
+		System.out.println("request home get");
+		return "index";
+
+	}
+
+	@RequestMapping("/")
+	public String requestCreatePageIndex2() {
+
+		return "redirect:/home";
+
+	}
 
 	@RequestMapping("/telechargement")
-    public String requestCreatePageTelechargement() {
-    	
-    	return "telechargement";
-    	
-    }
-	
-	
+	public String requestCreatePageTelechargement() {
+
+		return "telechargement";
+
+	}
+
 	@RequestMapping("/inscription")
-    public String requestCreatePageInscription() {
-    	
-    	return "inscription";
-    	
-    }
-	
+	public String requestCreatePageInscription() {
+
+		return "inscription";
+
+	}
+
 	@RequestMapping("/user_page")
-    public String requestCreatePageUserHome() {
-    	
+	public String requestCreatePageUserHome() {
+
 		System.out.println("user_page");
-    	return "user_page";
-    	
-    }
-	
-	
-	
+		return "user_page";
+
+	}
+
 	@RequestMapping("/tarification")
-    public String requestCreatePageTarification() {
-    	
-    	return "tarification";
-    	
-    }
+	public String requestCreatePageTarification() {
+
+		return "tarification";
+
+	}
 }
