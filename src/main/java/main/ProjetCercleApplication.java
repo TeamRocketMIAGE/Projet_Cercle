b2c99304f06d27d3a1704f6d23e27f2e6b1bcbaf
Christopher Girod <christopher.girod@gmail.com>
2015-10-06 13:50:48 +0200

    Multiples ajouts

--- src/main/java/main/ProjetCercleApplication.java
+++ src/main/java/main/ProjetCercleApplication.java
@@ -2,10 +2,22 @@ package main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
-public class ProjetCercleApplication {
+public class ProjetCercleApplication extends WebMvcConfigurerAdapter {
 
+	
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/inscription").setViewName("inscription");
+        registry.addViewController("/telechargement").setViewName("telechargement");
+        registry.addViewController("/user_page").setViewName("user_page");
+    }
+	
     public static void main(String[] args) {
         SpringApplication.run(ProjetCercleApplication.class, args);
     }
