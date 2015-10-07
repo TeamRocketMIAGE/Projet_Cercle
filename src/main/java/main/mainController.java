afd9e652df79c04bb532d95a64dc1ff412f40afc
Christopher Girod <christopher.girod@gmail.com>
2015-10-07 16:29:38 +0200

    401 et 500 opérationnel

--- src/main/java/main/mainController.java
+++ src/main/java/main/mainController.java
@@ -48,12 +48,11 @@ public class mainController {
 	public EmbeddedServletContainerCustomizer containerCustomizer() {
 	 
 	   return (container -> {
-		   ErrorPage error401Page = new ErrorPage(HttpStatus.UNAUTHORIZED, "/401.html");
-	        ErrorPage error404Page = new ErrorPage(HttpStatus.NOT_FOUND, "/404.html");
-	        ErrorPage error500Page = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/500.html");
+		    ErrorPage error500Page = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR,"/500");
+	        ErrorPage error404Page = new ErrorPage(HttpStatus.NOT_FOUND, "/404");
+	        ErrorPage error401Page = new ErrorPage(HttpStatus.UNAUTHORIZED,"/401");
 	 
-	 
-	        container.addErrorPages( error401Page, error404Page, error500Page);
+	        container.addErrorPages( error404Page,error500Page,error401Page);
 	   });
 	}
 	
